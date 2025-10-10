ca35904508f33697843a1b4fe57f1c5fbdd2f3f6
far1h <farihmuhammad45@gmail.com>
2025-10-10 21:56:19 +0700

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Farih Muhammad on 10/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
