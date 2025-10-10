4b00792c4dcab080836c1f07f154d010e61015c6
far1h <farihmuhammad45@gmail.com>
2025-10-10 23:41:07 +0700

    Add a New Budget Category

---
+++ BudgetApp/Extensions/Double+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Double+Extensions.swift
+//  BudgetApp
+//
+//  Created by Farih Muhammad on 10/10/2025.
+//
+
+import Foundation
+
+extension Double {
+    func toCurrency() -> String {
+        return self.formatted(.currency(code: "IDR"))
+    }
+}
